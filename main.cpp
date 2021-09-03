729ccacbe11b223e2b8e9bbb69d6da40c6cc829e
유병한 <90017857+cek516201@users.noreply.github.com>
2021-09-03 03:53:04 +0000

    덧셈

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* args[])
 {
-  printf("Hello SDL\n");
+  int a = 0;
+  int b = 0;
+
+  cin >> a >> b;
+  cout << a + b << "\n";
 
   return 0;
 }
