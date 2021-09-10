74a99c5e3e3664821bf7da19597db0cc98bf53c0
유병한 <90017857+cek516201@users.noreply.github.com>
2021-09-10 03:59:29 +0000

    2주차

--- main.cpp
+++ main.cpp
@@ -1,15 +1,78 @@
-#include <iostream>
+// 창을 3번 출력하며 각 창마다 옵션을 조금 바꾸어보았습니다
 
-using namespace std;
+#include <SDL.h>
 
-int main(int argc, char* args[])
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+void init(const char* title, int xpos, int ypos, 
+          int height, int width, int flags);
+void render();
+
+int main(int argc, char* argv[])
 {
-  int a = 0;
-  int b = 0;
+    //첫번째
+    init("Test1", 
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED, 
+          500, 100,
+          SDL_WINDOW_SHOWN);
 
-  cin >> a >> b;
-  cout << a + b << "\n";
-  cout << a + b << "\n";
+    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
+    render();
 
-  return 0;
+    SDL_Delay(1000);
+    SDL_Quit();
+
+
+    //두번째
+    init("Test2", 
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED, 
+          400, 200,
+          SDL_WINDOW_BORDERLESS);
+
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 255);
+    render();
+
+    SDL_Delay(1000);
+    SDL_Quit();
+    
+
+    //세번째
+    init("Test3", 
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED, 
+          300, 300,
+          SDL_WINDOW_MAXIMIZED);
+
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
+    render();
+
+    SDL_Delay(1000);
+    SDL_Quit();
+
+    return 0;
+}
+
+void init(const char* title, int xpos, int ypos, 
+          int height, int width, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+    {
+        g_pWindow = SDL_CreateWindow( title, 
+                                      xpos, ypos, 
+                                      height, width, flags);
+
+        if (g_pWindow != 0)
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+    }
+}
+
+void render()
+{
+    SDL_RenderClear(g_pRenderer);
+    SDL_RenderPresent(g_pRenderer);
 }
